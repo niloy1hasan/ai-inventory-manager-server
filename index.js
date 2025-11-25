9095e5296428a107ac2f0b0573316c5996f23293
niloy1hasan <notecse23@gmail.com>
2025-11-25 15:02:22 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kpocggx.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +22,19 @@ app.get("/", (req, res) => {
   res.send("AI Model Inventory Manager Server Running");
 });
 
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected successfully");
+  } finally {
+    console.log("MongoDB setup done");
+  }
+}
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+run().catch(console.dir);
